ce8a04f5050b29e9726fd2cf8abbc68f046e0356
julenb <21701719@live.uem.es>
2018-10-24 18:50:46 +0200

    WebView with loading spinner

--- app/src/main/java/ee/diagonal/alfamedic/MainActivity.java
+++ app/src/main/java/ee/diagonal/alfamedic/MainActivity.java
@@ -1,13 +1,69 @@
 package ee.diagonal.alfamedic;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ImageView imgAlfaMedic;
+    private ProgressBar progressBar;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.web_view);
+        webView.setVisibility(View.GONE);
+        imgAlfaMedic=findViewById(R.id.img_alfamedic);
+        progressBar=findViewById(R.id.progress_bar);
+        linearLayout=findViewById(R.id.header_progress);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int progress) {
+                linearLayout.setVisibility(View.VISIBLE);
+                if(progress == 100){
+                    webView.setVisibility(View.VISIBLE);
+                    imgAlfaMedic.setVisibility(View.GONE);
+                    linearLayout.setVisibility(View.GONE);
+                }
+            }
+        });
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.app.alfamedic.es/");
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
